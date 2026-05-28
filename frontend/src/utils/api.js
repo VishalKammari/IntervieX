ee50362d6b4d26bed31ceb28c2b87e91e7e2ac9d
Vishal Kammari <vishalkammari2102@gmail.com>
2026-05-28 19:19:44 +0530

    Update API base URL to new endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://interviex.onrender.com',
+  baseURL: 'https://interviex-wk7r.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
